1dd1e3a633b67545e885677ff1cd22cf14b5c570
Lemo <o.lemonakis@live.dk>
2025-05-07 22:58:50 +0200

    adding workflows

--- Assets/Scriptable Objects/CodeBlockType.cs
+++ Assets/Scriptable Objects/CodeBlockType.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CodeBlocks", menuName = "Scriptable Objects/CodeBlocks")]
 public class CodeBlockType : ScriptableObject
 {
+    public string codeToExecute;
     
 }
